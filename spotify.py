6f58902726f248b1b016ef396e731417ebb8561b
t3rm!nal <144577613+Terminal127@users.noreply.github.com>
2024-02-11 10:10:32 +0530

    Add files via upload

---
+++ spotify.py
@@ -0,0 +1,37 @@
+import json 
+import spotipy 
+import webbrowser 
+
+username = '31rnhfwcsabhbqyllyzybvuh7isi'
+clientID = '67ef37a8174741d588bb2ce9e239f976'
+clientSecret = 'ec836d192b544a7bb422793089e3c182'
+redirect_uri = 'http://google.com/callback/'
+oauth_object = spotipy.SpotifyOAuth(clientID, clientSecret, redirect_uri) 
+token_dict = oauth_object.get_access_token() 
+token = token_dict['access_token'] 
+spotifyObject = spotipy.Spotify(auth=token) 
+user_name = spotifyObject.current_user() 
+
+# To print the JSON response from 
+# browser in a readable format. 
+# optional can be removed 
+print(json.dumps(user_name, sort_keys=True, indent=4)) 
+
+while True: 
+	print("Welcome to the project, " + user_name['display_name']) 
+	print("0 - Exit the console") 
+	print("1 - Search for a Song") 
+	user_input = int(input("Enter Your Choice: ")) 
+	if user_input == 1: 
+		search_song = input("Enter the song name: ") 
+		results = spotifyObject.search(search_song, 1, 0, "track") 
+		songs_dict = results['tracks'] 
+		song_items = songs_dict['items'] 
+		song = song_items[0]['external_urls']['spotify'] 
+		webbrowser.open(song) 
+		print('Song has opened in your browser.') 
+	elif user_input == 0: 
+		print("Good Bye, Have a great day!") 
+		break
+	else: 
+		print("Please enter valid user-input.") 
