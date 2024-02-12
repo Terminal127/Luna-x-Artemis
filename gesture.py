1466073220b87bed66f7afb640de1ad807d754dd
t3rm!nal <144577613+Terminal127@users.noreply.github.com>
2024-02-13 01:51:29 +0530

    Update gesture.py

--- gesture.py
+++ gesture.py
@@ -13,16 +13,26 @@ mp_draw = mp.solutions.drawing_utils
 
 prev_hand_x = 0  
 movement_threshold = 200 
-movement_threshold_x = 200
+movement_threshold_x = 200# Adjust this value for sensitivity
 movement_threshold_exit = 500
-frame_count = 0  
-c = 0  
+frame_count = 0  # Counter to keep track of frames
+c = 0  # Gesture count
 
 def show_tabs():
+    cv2.CAP_DSHOW = 0
+    cap = cv2.VideoCapture(0)
     global prev_hand_y, gesture_count
-
-    gesture_count = 0
-    prev_hand_y = 0  
+    mp_hands = mp.solutions.hands
+    hands = mp_hands.Hands(static_image_mode=False, max_num_hands=2, min_detection_confidence=0.5, min_tracking_confidence=0.5)
+    mp_draw = mp.solutions.drawing_utils
+    prev_hand_x = 0  
+    movement_threshold = 100 
+    movement_threshold_x = 200# Adjust this value for sensitivity
+    movement_threshold_exit = 500
+    frame_count = 0  # Counter to keep track of frames
+    c = 0 
+    gesture_count = 0  # Initialize gesture count within the function
+    prev_hand_y = 0  # Initialize prev_hand_y at the beginning of the function
 
     while cap.isOpened():
         ret, frame = cap.read()
@@ -56,7 +66,20 @@ def show_tabs():
             break
 
 def swipe():
-    global prev_hand_x, frame_count
+    cv2.CAP_DSHOW = 0
+    cap = cv2.VideoCapture(0)
+    global prev_hand_y, gesture_count
+    mp_hands = mp.solutions.hands
+    hands = mp_hands.Hands(static_image_mode=True, max_num_hands=2, min_detection_confidence=0.5, min_tracking_confidence=0.7) # this value is chaned up fomr static value falso to true
+    mp_draw = mp.solutions.drawing_utils
+    prev_hand_x = 0  
+    movement_threshold = 200 
+    movement_threshold_x = 100# Adjust this value for sensitivity
+    movement_threshold_exit = 400
+    frame_count = 0  # Counter to keep track of frames
+    c = 0 
+    gesture_count = 0  # Initialize gesture count within the function
+    prev_hand_y = 0 
     prev_hand_x = 0
 
     while cap.isOpened():
@@ -84,6 +107,9 @@ def swipe():
                     cap.release()
                     cv2.destroyAllWindows()
                 prev_hand_x = index_tip_x
+                
+        
+        cv2.imshow('Hand Gesture', frame)
         
         
         if cv2.waitKey(1) == ord('q'):
@@ -117,6 +143,7 @@ def fast_swipe():
                     tabs_handler.select()                    
                     cap.release()
                     cv2.destroyAllWindows()
+                    sys.exit("exiting")
                 prev_hand_x = index_tip_x
                 
         
@@ -130,8 +157,8 @@ def text():
     print ("hello world")
 
 def main():
-    # show_tabs()
-    fast_swipe()
+    show_tabs()
+
     
 
 if __name__ == "__main__":
