9d748ce9de696505a63068c62927d1aa59d2c3ec
t3rm!nal <144577613+Terminal127@users.noreply.github.com>
2024-02-16 02:50:34 +0530

    Add files via upload

---
+++ airqality.py
@@ -0,0 +1,42 @@
+# uses aqi api
+import requests
+import json
+
+# Replace 'YOUR_API_KEY' with your actual API key
+api_key = 'AIzaSyDHbiWAHuck5ZYZ_D8hPDZiTnLyb1CsuqA'
+
+url = 'https://airquality.googleapis.com/v1/currentConditions:lookup?key=' + api_key
+
+# JSON data to be sent in the POST request
+data = {
+    "universalAqi": True,
+    "location": {
+        "latitude": 20.3555462,
+        "longitude": 85.7980762
+    },
+    "extraComputations": [
+        "HEALTH_RECOMMENDATIONS",
+        "DOMINANT_POLLUTANT_CONCENTRATION",
+        "POLLUTANT_CONCENTRATION",
+        "LOCAL_AQI",
+        "POLLUTANT_ADDITIONAL_INFO"
+    ],
+    "languageCode": "en"
+}
+
+# Headers for the request
+headers = {'Content-Type': 'application/json'}
+
+# Perform the POST request
+response = requests.post(url, json=data, headers=headers)
+
+# Check if the request was successful (status code 200)
+if response.status_code == 200:
+    print("Request successful!")
+
+    # Pretty print the JSON response
+    formatted_response = json.dumps(response.json(), indent=2)
+    print(formatted_response)
+else:
+    print(f"Request failed with status code: {response.status_code}")
+    print(response.text)
